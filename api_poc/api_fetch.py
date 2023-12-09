36206171ccd6f9310c2e8607e4b17a3ed6467f86
Hick <makoto.sato.m@gmail.com>
2023-12-09 10:12:20 +0000

    fix api_poc

--- api_poc/api_fetch.py
+++ api_poc/api_fetch.py
@@ -8,30 +8,30 @@ INSTAGRAM_ACCESS_TOKEN = os.getenv('INSTAGRAM_ACCESS_TOKEN')
 
 # ユーザープロフィールを取得する関数
 def get_user_profile():
+    # TODO: Instagram APIのURLを構築する
+    # ヒント: f-stringを使用してURLを構築する
     url = f"https://graph.facebook.com/v18.0/me?fields=id%2Cname&access_token={INSTAGRAM_ACCESS_TOKEN}"
+
+    # TODO: リクエストを送信し、レスポンスを取得する
+    # ヒント: requests.getを使用してリクエストを送信し、response変数に格納する
     response = requests.get(url)
+
+    # TODO: レスポンスが成功した場合はJSONデータを返す
+    # ヒント: response.status_codeが200の場合、response.json()を返す
     if response.status_code == 200:
         return response.json()
     else:
         raise Exception("API request failed with status code " + str(response.status_code))
 
-# データを処理して表示する関数
-def process_data(data):
-    print(data)
-    print("User ID:", data['id'])
-    print("Username:", data['name'])
-
-# エラーハンドリングを行う関数
-def handle_errors(e):
-    print("An error occurred:", e)
-
 # メイン関数
 def main():
     try:
+        # TODO: get_user_profile関数を呼び出し、結果を表示する
+        # ヒント: get_user_profile関数を呼び出し、その結果をprint関数で表示する
         profile = get_user_profile()
-        process_data(profile)
+        print(profile)
     except Exception as e:
-        handle_errors(e)
+        print("An error occurred:", e)
 
 if __name__ == "__main__":
     main()
--- api_poc/api_fetch_exercise.py
+++ api_poc/api_fetch_exercise.py
@@ -8,29 +8,30 @@ INSTAGRAM_ACCESS_TOKEN = os.getenv('INSTAGRAM_ACCESS_TOKEN')
 
 # ユーザープロフィールを取得する関数
 def get_user_profile():
-    url = f"https://graph.facebook.com/v18.0/me?fields=id%2Cname&access_token={INSTAGRAM_ACCESS_TOKEN}"
-    # ここを実装する
-    pass
+    # TODO: Instagram APIのURLを構築する
+    # ヒント: f-stringを使用してINSTAGRAM_ACCESS_TOKENを読み出してURLを構築する
+    url = f"https://graph.facebook.com/v18.0/me?fields=id%2Cname&access_token={}"
 
-# データを処理して表示する関数
-def process_data(data):
-    # ここを実装する
-    pass
+    # TODO: リクエストを送信し、レスポンスを取得する
+    # ヒント: requests.getを使用してリクエストを送信し、response変数に格納する
+    response = None
 
-# エラーハンドリングを行う関数
-def handle_errors(response):
-    # ここを実装する
-    pass
+    # TODO: レスポンスが成功した場合はJSONデータを返す
+    # ヒント: response.status_codeが200の場合、response.json()を返す
+    if response:
+        pass
+    else:
+        raise Exception("API request failed with status code " + str(response.status_code))
 
 # メイン関数
 def main():
     try:
-        # ユーザプロファイル取得
-        profile = get_user_profile()
-        # プロファイルを表示
-        process_data(profile)
+        # TODO: get_user_profile関数を呼び出し、結果を表示する
+        # ヒント: get_user_profile関数を呼び出し、その結果をprint関数で表示する
+        profile = None
+        print(profile)
     except Exception as e:
-        handle_errors(e)
+        print("An error occurred:", e)
 
 if __name__ == "__main__":
     main()
